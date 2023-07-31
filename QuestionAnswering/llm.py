47f800b698e92e801bcc7250130fe769a1d1bb75
Eashan Chawla <eashanchawla@gmail.com>
2023-07-30 22:29:52 -0500

    creating vectorstore and adding model code

---
+++ QuestionAnswering/llm.py
@@ -0,0 +1,17 @@
+from langchain.llms import CTransformers
+
+print('Loading LLAMA model...')
+llm = CTransformers(
+    model='models/llama-2-7b-chat.ggmlv3.q8_0.bin',
+    model_type='llama',
+    config={
+        'max_new_tokens': 500,
+        'temperature': 0.01
+    }
+)
+
+print('Generating text...')
+import time
+start = time.time()
+print(llm("AI is going to"))
+print('Time taken: {time.time() - start}.')
